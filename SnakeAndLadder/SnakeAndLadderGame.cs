74b0672ac1addc6a35f649b3befc5202d702c587
Devendra533 <97461548+Devendra533@users.noreply.github.com>
2022-02-14 17:17:35 +0530

    Added Checks For Opration Logic

--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -8,15 +8,39 @@ namespace SnakeAndLadder
 {
     internal class SnakeAndLadderGame
     {
+        public const int NO_PLAY = 1;
+        public const int LADDER = 2;
+        public const int SNAKE = 3;
+
+        public int position = 0;
+        Random number = new Random();
         public void PlayGame() 
         {
-            int position = 0;
-            Random number = new Random();
+           
             Console.WriteLine( "your position Is At :" + position);
-            int check = number.Next(1, 7);
-            Console.WriteLine( "You Get :" + check);
-            position += check;
-            Console.WriteLine("Your Position Is At :" + position);
+            int check1 = number.Next(1, 7);
+            int check2 = number.Next(1, 4);
+            switch (check2) 
+            {
+               case 1:
+                    Console.WriteLine("Your Position Is Same");
+                    Console.WriteLine("Your Position IS :" + position);
+                    break;
+                case 2:
+                    position += check1;
+                    Console.WriteLine("Congrats It's A Ladder");
+                    Console.WriteLine("Your Position Is :" + position);
+
+                    break;
+                case 3:
+                    Console.WriteLine("Sorry It's A Snake");
+                    Console.WriteLine("You Return TO :" + position);
+                    break;
+            }
+
+
+
+
 
         }
 
