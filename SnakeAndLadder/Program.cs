1b727b5a36bc0cd255e41edd020bb726c93393c1
Devendra533 <97461548+Devendra533@users.noreply.github.com>
2022-02-14 15:44:15 +0530

    Added Snake And Ladder Game Logic

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SnakeAndLadder;
 Console.WriteLine(" ......Welcome To Snake And Ladder Game .......");
+SnakeAndLadderGame play = new SnakeAndLadderGame();
+play.PlayGame();
+
 Console.ReadKey();
---
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadderGame
+    {
+        public void PlayGame() 
+        {
+            int position = 0;
+            Console.WriteLine( "position :" + position);
+
+
+        }
+
+       
+    }
+}
